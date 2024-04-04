12fb3bfcdddf5aec43f2a21064db4bcdadcd9262
reumy <549747168@qq.com>
2024-04-05 00:47:45 +0800

    add kv to track uploaded photos

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/cloudflare-workers";
 
-const app = new Hono<{ Bindings: { API_HOST: string } }>();
+type Bindings = {
+  API_HOST: string;
+  universe: KVNamespace;
+};
+
+const app = new Hono<{
+  Bindings: Bindings;
+}>();
 
 app.get("/*", serveStatic({ root: "./" }));
 
@@ -10,14 +17,33 @@ app.post("/upload", async (c) => {
   const file = body.file as File;
   const formData = new FormData();
   formData.append("file", file, file.name);
-  return fetch(`${c.env.API_HOST}/upload`, {
+  const res = await fetch(`${c.env.API_HOST}/upload`, {
     method: "POST",
     body: formData,
   });
+
+  const resClone = res.clone();
+
+  const data = (await resClone.json()) satisfies Array<{ src: string }>;
+  const tgphPath = data[0].src.split("/").pop();
+  if (!tgphPath) throw new Error("tgphPath not found");
+
+  // // upload to KV
+  // // key: filename, value: tgphName
+  // // @ts-ignore
+  await c.env.universe.put(file.name.split(".")[0], tgphPath);
+
+  return res;
 });
 
-app.get("/file/:name", (c) => {
-  return fetch(`${c.env.API_HOST}/file/${c.req.param("name")}`);
+app.get("/file/:tgphPath", (c) => {
+  return fetch(`${c.env.API_HOST}/file/${c.req.param("tgphPath")}`);
+});
+
+app.get("/kv/:name", async (c) => {
+  // @ts-ignore
+  const tgphName = await c.env.universe.get(c.req.param("name"));
+  return fetch(`${c.env.API_HOST}/file/${tgphName}`);
 });
 
 export default app;
